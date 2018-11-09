d67bffcbb4f2b11dfeef26f967439d09da2c978b
Terry Seidler <terry.seidler@ogd.nl>
2018-11-09 20:19:26 +0000

    Add service worker (basic)

---
+++ src/coconut_service_worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', event => {
+  console.info('Installing service worker');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
